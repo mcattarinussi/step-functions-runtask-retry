109dd7bfb217e8cfb952a9808d5ae65156aa5765
Mattia Cattarinussi <mcattarinussi@gmail.com>
2019-10-06 18:15:23 +0100

    limit number of task retries

--- src/process-error.js
+++ src/process-error.js
@@ -1,6 +1,18 @@
+const MAX_RETRIES = 3;
+
 module.exports.handler = async event => {
     console.log('Received event', JSON.stringify(event));
+    const retryCount = event.errorState ? event.errorState.retryCount : 0;
+
+    if (retryCount >= MAX_RETRIES) {
+        return {
+            type: 'FATAL'
+        };
+    }
+
     return {
-        type: event.injectedErrorType
+        type: event.injectedErrorType,
+        waitSeconds: 10,
+        retryCount: retryCount + 1
     }
 }
